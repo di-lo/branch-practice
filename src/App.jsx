4a7d427f42466d479a0a935b38a5e610881ee2af
Didi <dilaraishik@gmail.com>
2026-06-06 15:05:18 +0300

    button

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
             <li>
               <a href="https://react.dev/" target="_blank">
                 <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
+                Click Me & Learn more
               </a>
             </li>
           </ul>
